d88c90c9775c7e32a4245c943c769e56a98f8826
kristin <kristinbjny@gmail.com>
2021-05-19 07:51:59 -0700

    start a infeerence web service

--- train/predict_aws.py
+++ train/predict_aws.py
@@ -7,11 +7,11 @@ import redis
 import json
 from util import text2vec
 import time
-import boto3
+from sagemaker import KMeansPredictor
 
 # grab environment variables
 ENDPOINT_NAME = os.environ['ENDPOINT_NAME']
-runtime= boto3.client('runtime.sagemaker')
+kmeans_endpoint = KMeansPredictor(ENDPOINT_NAME)
 
 PREDICT_Q="predict_q"
 r = redis.Redis()
@@ -19,37 +19,32 @@ r = redis.Redis()
 print("Loading Google pre-trained model")
 model = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
 
-# Load from file
+# define sagemaker K-means endpoint
 print("define sagemaker K-means endpoint")
 def sagemaker_kmeans_predict(vec):
-  payload = ",".join(vec)
-  response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
-                                        ContentType='text/csv',
-                                        Body=payload)
-  print("predict: {}".format(response))
-  return response
+    response = kmeans_endpoint.predict(vec)
+    #print(response[0].label)
+    return response[0].label.get("closest_cluster").float32_tensor.values[0]
+
+def infer_cluster(text):
+  vec = text2vec(model, text)
+  return [sagemaker_kmeans_predict(vec)[0], vec]
+  
 
 print("Loading news data source")
 pkl_filename = "news_df.pkl"
 with open(pkl_filename, 'rb') as file:
     news_df = pickle.load(file)
 
-def infer_cluster(text):
-  vec = text2vec(model, text)
-  return [sagemaker_kmeans_predict(vec), vec]
-
 def recommend_news(text):
   [predicted_clustor, vec] = infer_cluster(text)
   recommends = pd.DataFrame([])
-  news_ids = []
   for index, row in news_df.iterrows():
     if row["cluster"] == predicted_clustor:
       recommends = recommends.append(row)
-      # TODO: replace with news id
-      news_ids.append(str(row["title"]))
       if len(recommends) > 10:
         break
-  return str(predicted_clustor[0]), news_ids
+  return str(predicted_clustor[0]), recommends
 
 print("Recommendation start up complete")
 while True:
@@ -60,10 +55,10 @@ while True:
         rid = rid.decode("utf-8")
         print(text)
         # Calculate the accuracy score and predict target values
-        [cluster, news_ids] = recommend_news(text)
+        [cluster, recommends] = recommend_news(text)
         print(cluster)
-        print(news_ids)
-        res = json.dumps({"cluster": cluster, "news_ids": news_ids})
+        print(recommends)
+        res = json.dumps({"cluster": cluster, "news": recommends.to_json()})
         r.set(rid + "res", res)
     time.sleep(0.1)
 
